1f71ea08e3a0ceb9cf099b68729ee7b8401de61e
unknown <awanm6815@gmail.com>
2026-09-11 19:31:29 +0500

    feat: Add resource routes, controller and toggle route

--- routes/web.php
+++ routes/web.php
@@ -1,7 +1,13 @@
 <?php
 
+use App\Http\Controllers\TaskController;
 use Illuminate\Support\Facades\Route;
 
 Route::get('/', function () {
-    return view('welcome');
+    return redirect()->route('tasks.index');
 });
+
+Route::resource('tasks', TaskController::class);
+
+Route::patch('tasks/{task}/toggle', [TaskController::class, 'toggleStatus'])
+    ->name('tasks.toggle');
